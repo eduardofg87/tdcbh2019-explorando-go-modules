36f5ed9e098a1d0f25547f81f04a99fcb7a4cc2c
eduardofg87 <goncalves.ef@gmail.com>
2019-06-15 15:20:14 -0300

    finished

--- Dockerfile
+++ Dockerfile
@@ -19,5 +19,6 @@ FROM scratch
 COPY --from=builder /app/tdcbh2019-explorando-go-modules /app/
 #export go.png
 COPY go.png /app/
+COPY tdc_logo.png /app/
 EXPOSE 8080
 ENTRYPOINT ["/app/tdcbh2019-explorando-go-modules"]
--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	//"os/exec"
 	"github.com/GuilhermeCaruso/bellt"
-	//"github.com/eduardofg87/img2ascii_go"
 	"github.com/qeesung/image2ascii/convert"
 	_ "image/jpeg"
 	_ "image/png"
@@ -16,49 +14,23 @@ func main() {
 
 	router := bellt.NewRouter()
 
-	router.HandleFunc("/contact/{id}/{user}", bellt.Use(
-		exampleHandler,
+	router.HandleFunc("/gopher", bellt.Use(
+		gopherHandler,
 		middlewareOne,
 		middlewareTwo,
 	), "GET")
 
-	router.HandleFunc("/tdc", bellt.Use(
-		exampleNewHandler,
+	router.HandleFunc("/tdc-logo", bellt.Use(
+		tdcLogoHandler,
 		middlewareOne,
 		middlewareTwo,
 	), "GET")
-
-	router.HandleGroup("/api",
-		router.SubHandleFunc("/check", bellt.Use(
-			exampleNewHandler,
-			middlewareOne,
-			middlewareTwo,
-		), "GET"),
-		router.SubHandleFunc("/check/{id}/{user}", bellt.Use(
-			exampleHandler,
-			middlewareOne,
-			middlewareTwo,
-		), "GET"),
-	)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func exampleHandler(w http.ResponseWriter, r *http.Request) {
 
-	rv := bellt.RouteVariables(r)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"id": %v, "user": %v}`, rv.GetVar("user"), rv.GetVar("id"))))
-}
-
-func exampleNewHandler(w http.ResponseWriter, r *http.Request) {
-	//rv := bellt.RouteVariables(r)
-	
-	//output, _ := exec.Command("image2ascii_go -f go.png").Output()
-
-    //fmt.Println(output)
+func gopherHandler(w http.ResponseWriter, r *http.Request) {
 	convertOptions := convert.DefaultOptions
 	convertOptions.FixedWidth = 100
 	convertOptions.FixedHeight = 40
@@ -72,10 +44,23 @@ func exampleNewHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"msg": "Gopher printed!"}`))
 }
 
+
+func tdcLogoHandler(w http.ResponseWriter, r *http.Request) {
+	convertOptions := convert.DefaultOptions
+	convertOptions.FixedWidth = 100
+	convertOptions.FixedHeight = 40
+
+	// Create the image converter
+	converter := convert.NewImageConverter()
+	imageFilename := "tdc_logo.png"
+	fmt.Print(converter.ImageFile2ASCIIString(imageFilename, &convertOptions))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"msg": "TDC Logo printed!"}`))
+}
 func middlewareOne(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Step One")
-
 		next.ServeHTTP(w, r)
 	}
 }
@@ -83,7 +68,6 @@ func middlewareOne(next http.HandlerFunc) http.HandlerFunc {
 func middlewareTwo(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Step Two")
-
 		next.ServeHTTP(w, r)
 	}
 }
